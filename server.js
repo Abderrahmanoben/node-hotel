4e78d9be7fc69886707592a88e916aca2db2ac6b
OCC-0111 <occ-0111@occ-0111s-mbp.home>
2023-09-08 12:17:37 +0200

    level one finished

--- server.js
+++ server.js
@@ -13,6 +13,47 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+//create a new booking
+app.post("/bookings", function (req, res) {
+  const newBooking = req.body;
+  // if (newBooking = "") {
+  //   res.status(400).send("you cannot submit an empty booking!")
+  // }
+  bookings.push(newBooking);
+  res.status(201).send(newBooking);
+});
+
+//read all bookings
+app.get("/bookings", function (req, res) {
+  res.send(bookings);
+});
+
+//read one booking, specified by an ID
+app.get("/bookings/:id", function (req, res) {
+  const requestedId = req.params.id;
+  const filteredById = bookings.find((element) => element.id == requestedId);
+  if (!filteredById) {
+    return res
+      .status(404)
+      .send(`The booking with id number ${requestedId} was not found!`)
+  };
+  res.send(filteredById);
+});
+
+//Delete a booking, specified by an ID
+app.delete("/bookings/:id", (req, res) => {
+  const requestedId = req.params.id;
+  const filteredById = bookings.find((element) => element.id == requestedId);
+  if (!filteredById){
+    return res
+    .status(404)
+    .send(`The booking with id number ${requestedId} was not found!`)
+  };
+  const findElementIndex = bookings.findIndex((element) => element.id == requestedId);
+  bookings.splice(findElementIndex, 1);
+  res.status(200).send(`The booking with id number ${requestedId} was succesfully deleted.`);
+});
+
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
